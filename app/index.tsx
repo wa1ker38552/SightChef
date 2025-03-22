88ad624fcde708277da22fbaac8303144d596e45
EmmetAVS <aarush.shrikanth@gmail.com>
2025-03-22 10:00:36 -0700

    app setup

---
+++ app/index.tsx
@@ -0,0 +1,4 @@
+import { registerRootComponent } from 'expo';
+import App from './App';  // Import the App component from App.js
+
+registerRootComponent(App);  // Register the App component as the root of the app
