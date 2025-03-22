b7705ee7d0e64a41b1652fe95eaaeb7f2f09ed0e
wa1ker38552 <walker.alt.38552@gmail.com>
2025-03-22 15:59:35 -0700

    ui changes

--- server/test.py
+++ server/test.py
@@ -1,4 +1,27 @@
 import requests
 
-response = requests.post('http://127.0.0.1:8002/api/process', files={'image': open('pantry.png', 'rb')})
-print(response.json())
+'''response = requests.post('http://127.0.0.1:8002/api/process', files={'image': open('pantry.png', 'rb')})
+print(response.json())'''
+
+import json
+
+headers = {
+    'Accent': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'Accent-Encodings': 'gzip, deflate, br, zstd',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Cache-Control': 'max-age=0',
+    'Origin': 'https://cosylab.iiitd.edu.in',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+    'Sec-Fetch-Site': 'same-origin',
+    'Referer': 'https://cosylab.iiitd.edu.in/recipedb/'
+}
+data = {
+    'page': 1,
+    'autocomplete_region': '',
+    'autocomplete_cuisine': '',
+    'autocomplete_recipe': ''
+}
+r = requests.post('https://cosylab.iiitd.edu.in/recipedb/search_recipe', headers=headers, data=data)
+
+with open('test.html', 'w') as file:
+    file.write(r.text)
