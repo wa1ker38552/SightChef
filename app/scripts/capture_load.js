9c62e7974b1061b1f50dee85a38aadde5e7ce7e8
wa1ker38552 <walker.alt.38552@gmail.com>
2025-03-22 14:35:08 -0700

    update

--- app/scripts/capture_load.js
+++ app/scripts/capture_load.js
@@ -1,6 +1,55 @@
+function processResponse(data) {
+    console.log(data)
+}
 
+function uploadFile() {
+    fileInput.click()
+}
+
+function uploadCamera() {
+    const video = dquery("#camera")
+    const canvas = dquery("#canvas")
+    const context = canvas.getContext("2d")
+    
+    const videoWidth = video.videoWidth;
+    const videoHeight = video.videoHeight;
+
+    const canvasWidth = canvas.width;
+    const canvasHeight = canvas.height;
+
+    const videoAspectRatio = videoWidth / videoHeight;
+    const canvasAspectRatio = canvasWidth / canvasHeight;
+
+    let drawWidth, drawHeight, offsetX = 0, offsetY = 0;
+
+    if (videoAspectRatio > canvasAspectRatio) {
+        drawWidth = videoWidth;
+        drawHeight = videoWidth / canvasAspectRatio;
+        offsetY = (drawHeight - videoHeight) / 2 * -1;
+    } else {
+        drawHeight = videoHeight;
+        drawWidth = videoHeight * canvasAspectRatio;
+        offsetX = (drawWidth - videoWidth) / 2 * -1;
+    }
+    context.drawImage(video, offsetX, offsetY, drawWidth, drawHeight, 0, 0, canvasWidth, canvasHeight);
+    const base64Image = canvas.toDataURL("image/jpeg")
+
+    fetch("http://localhost:8002/api/process", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ image: base64Image })
+    })
+        .then(response => response.json())
+        .then(response => {
+            processResponse(response)
+        })
+}
+
+var fileInput
 window.onload = function() {
+    fileInput = dquery("#fileUpload")
     const video = dquery('#camera');
+
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({ video: true })
         .then((stream) => {
@@ -9,5 +58,20 @@ window.onload = function() {
         .catch((error) => {
             console.error("Error accessing the camera: ", error);
         });
+    }
+
+    fileInput.onchange = function(e) {
+        const file = fileInput.files[0];
+        let formData = new FormData();
+        formData.append('image', file);
+
+        fetch("http://localhost:8002/api/process", {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.json())
+            .then(response => {
+                processResponse(response)
+            })
     }
 }
--- server/app.py
+++ server/app.py
@@ -1,9 +1,13 @@
+from flask_cors import CORS
 from flask import request
 from flask import Flask
 from PIL import Image
+import base64
+import io
 
 
 app = Flask(__name__)
+CORS(app)
 version = '0.0.0'
 
 @app.route('/')
@@ -12,7 +16,14 @@ def app_index():
 
 @app.route('/api/process', methods=['POST'])
 def api_process():
-    img = Image.open(request.files['image'])
+    ct = request.headers.get('Content-Type')
+    if 'json' in ct:
+        data = request.json
+        data['image'] = data['image'].replace('data:image/jpeg;base64,', '')
+        img = Image.open(io.BytesIO(base64.b64decode(data['image'])))
+    else:
+        img = Image.open(request.files['image'])
+    img.show()
     return {'success': True}
 
 app.run(host='0.0.0.0', port=8002)
