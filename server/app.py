13b319edc545a6dac1a473d153e003fcf0a37572
wa1ker38552 <walker.alt.38552@gmail.com>
2025-03-22 09:45:04 -0700

    server

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
 from flask import request
 from flask import Flask
+from PIL import Image
+
 
 app = Flask(__name__)
 version = '0.0.0'
@@ -7,5 +9,10 @@ version = '0.0.0'
 @app.route('/')
 def app_index():
     return f'v{version}'
+
+@app.route('/api/process', methods=['POST'])
+def api_process():
+    img = Image.open(request.files['image'])
+    return {'success': True}
 
 app.run(host='0.0.0.0', port=8002)
